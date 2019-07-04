de2b9880542896576954a3a0242bc58d3a371f36
Jan Kaiser <2159464+jankais3r@users.noreply.github.com>
2019-07-04 16:46:35 +0200

    Update stringEXEC.sql

--- stringEXEC.sql
+++ stringEXEC.sql
@@ -1,8 +1,9 @@
 
 CREATE FUNCTION [dbo].[stringEXEC] (@code NVARCHAR(4000))
-RETURNS NVARCHAR(250)
+RETURNS VARCHAR(4)
 AS
 BEGIN
+	-- Update the server hostname and the database name below. You can also supply login credentials with -U and -P parameters
 	DECLARE @sql NVARCHAR(4000) = 'sqlcmd -S SQLSERVER -d AdventureWorks -Q "' + @code + '"'
 	EXEC xp_cmdshell @sql
 	RETURN 'Done'
