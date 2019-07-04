dbe02b91fc4fcc668e750c783738498117e82b85
Jan Kaiser <2159464+jankais3r@users.noreply.github.com>
2019-07-04 11:57:23 +0200

    Update stringEXEC.sql

--- stringEXEC.sql
+++ stringEXEC.sql
@@ -3,7 +3,7 @@ CREATE FUNCTION [dbo].[stringEXEC] (@code NVARCHAR(4000))
 RETURNS NVARCHAR(250)
 AS
 BEGIN
-	DECLARE @sql NVARCHAR(4000) = 'sqlcmd -S SQLSERVER -d AdventrueWorks -Q "' + @code + '"'
+	DECLARE @sql NVARCHAR(4000) = 'sqlcmd -S SQLSERVER -d AdventureWorks -Q "' + @code + '"'
 	EXEC xp_cmdshell @sql
 	RETURN 'Done'
 END
