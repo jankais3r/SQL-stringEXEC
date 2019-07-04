bd6b28faf0efb77d82471030034ce3b6f234c01b
Jan Kaiser <2159464+jankais3r@users.noreply.github.com>
2019-07-04 10:11:04 +0200

    Add files via upload

---
+++ stringEXEC.sql
@@ -0,0 +1,9 @@
+
+CREATE FUNCTION [dbo].[stringEXEC] (@code NVARCHAR(4000))
+RETURNS NVARCHAR(250)
+AS
+BEGIN
+	DECLARE @sql NVARCHAR(4000) = 'sqlcmd -S SQLSERVER -d AdventrueWorks -Q "' + @code + '"'
+	EXEC xp_cmdshell @sql
+	RETURN 'Done'
+END
